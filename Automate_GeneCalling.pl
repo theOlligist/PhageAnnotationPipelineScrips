edd90f160086f1cf7ff84e101d310ae130443fa0
Gerid A Ollison <50889111+theOlligist@users.noreply.github.com>
2024-03-07 16:03:14 -0500

    Update Automate_GeneCalling.pl

--- Automate_GeneCalling.pl
+++ Automate_GeneCalling.pl
@@ -1,22 +1,14 @@
 #! /usr/bin/perl -w
 
-#This will be used for $i for each individual sample
-#open INFILE, "prefix_odd.txt";
-#open INFILE, "prefix_file.txt";
-open INFILE, "prefix_put1.txt";
+#This will be used for $i for each line in the prefix file/each genome prefix
+open INFILE, "prefix_odd.txt";
+
 @prefix = ();
 while (<INFILE>)
 { chomp;
   push(@prefix, $_);
 }
 close INFILE;
-
-#Perl script to automate V4 tag sequencing QC process
-#last updated 09-29-2017,  Sarah K. Hu
-# First step is to perform an initial QC. This step will take as input FWD and REV reads, and will output Quality filtered reads with and without mates, PE vs SE respectively.
-#print "source activate qiime1\n\n";
-#print "perl create_map.pl\n\n";
-#print "mv *_map.txt* mapfiles/\n\n";
 
 for $i(@prefix) {
 ##Place print statements of tasks to execute within this loop.
